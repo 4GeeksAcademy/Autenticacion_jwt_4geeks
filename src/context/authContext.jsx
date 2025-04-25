a69a53f5c779b5b5cd16d1da38e2fd46689c404c
Martín <martinjcaro@gmail.com>
2025-04-25 17:20:25 +0000

    private

---
+++ src/context/authContext.jsx
@@ -0,0 +1,16 @@
+// src/context/authContext.jsx
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null); // guardaremos el usuario aquí
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
---
+++ src/front/pages/Private.jsx
@@ -0,0 +1,13 @@
+// src/front/pages/Private.jsx
+
+import React from "react";
+
+export const Private = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Zona Privada</h1>
+      <p>Bienvenido a tu sesión privada 🎉</p>
+    </div>
+  );
+};
+
